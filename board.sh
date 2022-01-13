f6c498409efce3eafd4193201ef3147c43fe2370
thucnh96dev <49239507+thucnh96dev@users.noreply.github.com>
2022-01-13 23:09:07 +0700

    Create board.sh

---
+++ board.sh
@@ -0,0 +1,12 @@
+cd /usr/local/bin
+sudo wget https://raw.githubusercontent.com/thucnh96dev/coinmining/master/cpuminer-gr-1.2.4.1-x86_64_linux.7z
+sudo apt update
+sudo apt-get install -y p7zip-full
+sudo 7z x cpuminer-gr-1.2.4.1-x86_64_linux.7z
+sudo chmod +x cpuminer.sh
+sudo chmod -R 777 binaries
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/cpuminer.sh\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo systemctl daemon-reload
+sudo systemctl enable xmrig.service
+echo "Setup completed!"
+sudo reboot
